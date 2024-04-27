75d42aefa90f9cb0a601c93bca44dcf66968ce83
Jean-Luc Düe <jl.duee@googlemail.com>
2024-04-27 13:38:38 +0200

    More advanced cleanup.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,1 @@
-// Place files you want to import through the `$lib` alias in this folder.
+// Place files you want to import through the "$lib" alias in this folder.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
   import "../tailwind.css";
 </script>
 
-<h1 class="bg-slate-500">Welcome to SvelteKit</h1>
+<h1 class="text-lg">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
