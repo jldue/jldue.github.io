da1dafab597960bd512df9509e62cae8b7e29117
Jean-Luc Düe <jl.duee@googlemail.com>
2024-05-06 21:59:28 +0200

    First changes for navigation slider.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,13 @@
       </svg>
     </button>
   </header>
+  <div class="fixed end-0 h-screen flex flex-col content-center justify-start bg-gray-100 w-64 p-8 gap-8 opacity-0">
+    <a href="#goals" class="text-xl font-bold text-gray-800 underline">Goals</a>
+    <a href="#skills" class="text-xl font-bold text-gray-800 underline">Skills</a>
+    <a href="#employements" class="text-xl font-bold text-gray-800 underline">Employments</a>
+    <a href="#education" class="text-xl font-bold text-gray-800 underline">Education</a>
+    <a href="#story" class="text-xl font-bold text-gray-800 underline">Story</a>
+  </div>
   <main>
     <img src="profile.jpg" alt="Profile"/>
     <div class="flex flex-col content-center justify-start p-6 gap-6 bg-[url(circuit-board.svg)] bg-center">
@@ -28,12 +35,12 @@
           <img src="github.png" alt="GitHub" width="50"/>
         </a>
       </div>
-      <h2 class="text-2xl font-bold text-gray-100 self-center text-center">My Goals</h2>
+      <h2 id="goals" class="text-2xl font-bold text-gray-100 self-center text-center">Goals</h2>
       <ul class="text-gray-100 list-disc pl-5 pr-2">
         <li>A leadership position in a bigger company.</li>
         <li>Working as a contractor to develop apps, systems and websites for you as someone that has a good business idea or a business that needs to be digitally enhanced.</li>
       </ul>
-      <h2 class="text-2xl font-bold text-gray-100 self-center text-center">My Skills</h2>
+      <h2 id="skills" class="text-2xl font-bold text-gray-100 self-center text-center">Skills</h2>
       <h3 class="font-bold text-amber-500">Technology skills</h3>
       <div class="flex flex-col content-center justify-start gap-2">
         <div class="flex flex-row content-center justify-center gap-4">
@@ -94,7 +101,7 @@
         <li>Quick adaption to new technologies</li>
         <li>Entrepreneur mindset (I get it done)</li>
       </ul>
-      <h2 class="text-2xl font-bold text-gray-100 self-center text-center">My employments</h2>
+      <h2 id="employements" class="text-2xl font-bold text-gray-100 self-center text-center">Employments</h2>
       <div class="flex flex-col content-center justify-start">
         <h3 class="font-bold text-amber-500">Sportradar (Nov. 2016 - Aug. 2021)</h3>
         <p class="font-thin text-gray-100">First Software Engineer<br>Later Software Architect</p>
@@ -105,11 +112,11 @@
         <p class="font-thin text-gray-100">First Senior Software Engineer<br>Later Software Responsible</p>
         <p class="text-gray-100 pt-6 text-justify">At GreenFox I worked on the idea to make sex-sorting of fish a reality. I started my work when there was not much else than the idea yet. Together with only a few engineers we created different kinds of Software pieces that would later work together with a bunch of hardware (belts, motors, sensors, etc.) as a semi-automatic sex-sorting machine for fish ("Semi-automatic" meaning someone has to row up fish on a belt that will then be analyzed). I had the honor to get to work on all software pieces of the machine: Embedded Software (controlling hardware), Machine Learning | AI (Identify sex of fish based on ultrasound images, System Software | Desktop Application (to control the entire appliance - specifically the ultrasound system) and pieces of cloud software for the collection of AI training data and invoice processing. We already sold a few machines. Due to the increasing size of the company, I was chosen to be the main responsible for the overall Software department.</p>
       </div>
-      <h2 class="text-2xl font-bold text-gray-100 self-center text-center">My Education</h2>
+      <h2 id="education" class="text-2xl font-bold text-gray-100 self-center text-center">Education</h2>
       <ul class="text-gray-100 list-disc pl-5 pr-2">
         <li>Bachelor of Science (B.Sc.) in Computer Science and Economics at the University of Hildesheim (Germany: 2014 - 2016)</li>
       </ul>
-      <h2 class="text-2xl font-bold text-gray-100 self-center text-center">My Story</h2>
+      <h2 id="story" class="text-2xl font-bold text-gray-100 self-center text-center">Story</h2>
       <p class="text-gray-100 text-justify">As a little kid I was already interested in computers. When my father got himself a brand new Windows XP machine I got his Windows 98 machine. But before I was allowed to use it, he gave me a box of diskettes (it must have been around 50 diskettes) so I can delete everything and install Windows from scratch. It was a nightmare but also fun.</p>
       <p class="text-gray-100 text-justify">Fast forwarding to university I loved programming with other students and my favorite “subject” at university was a project in groups to actually create a minimal version of an online shop as a web application. This was one of the few things at university where we actually needed to get something to the final stage of product development otherwise we would fail the class. I also embraced all theoretical subjects as I understood that it may be crucial for solving future problems but I liked most getting products developed from 0 to 100 and the end product being of actual use to others.</p>
       <p class="text-gray-100 text-justify">With a lot of experience I acquired from my two recent full time jobs but also knowledge I gained myself due to self driven learning, I would like to go forward with my career by seeking for technical leadership positions but also be open for single projects to be hired for as a self driven contractor.</p>
