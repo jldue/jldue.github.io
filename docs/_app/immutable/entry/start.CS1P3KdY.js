87ccb070c2abbfe23f2b36bcdf9cde3bfac46da2
Jean-Luc Düe <jl.duee@googlemail.com>
2024-07-01 11:46:24 +0200

    Another try of building pages properly.

--- docs/_app/immutable/entry/start.CS1P3KdY.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DGO7FBnu.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,10 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-    })
+    }),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/jldue.github.io" : ""
+    }
   },
   preprocess: vitePreprocess()
 };
